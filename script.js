b61f37f2cbb8e609bedc89211289128a12174abd
volkan <volkandogru11@gmail.com>
2022-08-26 15:21:36 +0300

    shop detail added

--- script.js
+++ script.js
@@ -5,13 +5,35 @@ const menuCloseBtn = document.querySelector("#close");
 
 const navbarDom = document.querySelector("#navbar");
 
-menuOpenBtn.addEventListener("click" , menuOpen);
-menuCloseBtn.addEventListener("click" , menuClose);
+menuOpenBtn.addEventListener("click", menuOpen);
+menuCloseBtn.addEventListener("click", menuClose);
 
-function menuOpen(){
-    navbarDom.style.right="0px"
+function menuOpen() {
+  navbarDom.style.right = "0px";
 }
 
-function menuClose(){
-    navbarDom.style.right="-300px"
+function menuClose() {
+  navbarDom.style.right = "-300px";
 }
+
+const MainImgDom = document.querySelector("#MainImg");
+const smallImgDom = document.getElementsByClassName("smallImg");
+
+smallImgDom[0].onmouseover=function(){
+  MainImgDom.src = smallImgDom[0].src; 
+}
+smallImgDom[1].onmouseover=function(){
+  MainImgDom.src = smallImgDom[1].src; 
+}
+smallImgDom[2].onmouseover=function(){
+  MainImgDom.src = smallImgDom[2].src; 
+}
+smallImgDom[3].onmouseover=function(){
+  MainImgDom.src = smallImgDom[3].src; 
+}
+      
+
+
+
+
+
